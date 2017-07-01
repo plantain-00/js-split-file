52c1569e5a58a62721f3f91ff144eb6699c0764a
york yao <yaoao12306@outlook.com>
2017-07-02 00:14:48 +0800

    better unit test

--- spec/spec.ts
+++ spec/spec.ts
@@ -1,19 +1,15 @@
 import { SplitFile } from "../nodejs";
 
-describe("SplitFile", () => {
-    it("should work", () => {
-        const splitFile = new SplitFile();
-        const uint8Array = new Uint8Array([1, 2, 3, 4]);
-        const blocks = splitFile.split(uint8Array, "a.txt");
-        // tslint:disable-next-line:no-console
-        console.log(blocks);
-        const piece = splitFile.decodeBlock(blocks[0]);
-        // tslint:disable-next-line:no-console
-        console.log(piece);
-        expect(piece.totalBytesCount).toEqual(4);
-        expect(piece.fileName).toEqual("a.txt");
-        expect(piece.totalBlockCount).toEqual(1);
-        expect(piece.currentBlockIndex).toEqual(0);
-        expect(piece.binary).toEqual(new Uint8Array([1, 2, 3, 4]));
+it("", () => {
+    const splitFile = new SplitFile();
+    const blocks = splitFile.split(new Uint8Array([1, 2, 3, 4]), "a.txt");
+    expect(blocks).toEqual([new Uint8Array([4, 0, 0, 0, 5, 0, 0, 0, 97, 46, 116, 120, 116, 1, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4])]);
+    const piece = splitFile.decodeBlock(blocks[0]);
+    expect(piece).toEqual({
+        totalBytesCount: 4,
+        fileName: "a.txt",
+        totalBlockCount: 1,
+        currentBlockIndex: 0,
+        binary: new Uint8Array([1, 2, 3, 4]),
     });
 });
