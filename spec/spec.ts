3d5d521f71283c4b5ff97933fd8733f91cd17146
york yao <yaoao12306@outlook.com>
2017-07-01 16:01:37 +0800

    improve lint

--- spec/spec.ts
+++ spec/spec.ts
@@ -5,8 +5,10 @@ describe("SplitFile", () => {
         const splitFile = new SplitFile();
         const uint8Array = new Uint8Array([1, 2, 3, 4]);
         const blocks = splitFile.split(uint8Array, "a.txt");
+        // tslint:disable-next-line:no-console
         console.log(blocks);
         const piece = splitFile.decodeBlock(blocks[0]);
+        // tslint:disable-next-line:no-console
         console.log(piece);
         expect(piece.totalBytesCount).toEqual(4);
         expect(piece.fileName).toEqual("a.txt");
