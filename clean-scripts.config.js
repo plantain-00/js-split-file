d42d4c6d0758dcb810ed9e5746246d692c48c7f1
york yao <yaoao12306@outlook.com>
2017-07-28 00:10:25 +0800

    clean scripts

---
+++ clean-scripts.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  build: [
+    `rimraf dist/`,
+    `tsc -p src/tsconfig.nodejs.json`,
+    `tsc -p src/tsconfig.browser.json`,
+    `rollup --config rollup.config.js`
+  ],
+  lint: [
+    `tslint "*.ts" "spec/*.ts"`,
+    `standard "**/*.config.js"`,
+    `stylelint "online/**/*.less"`
+  ],
+  test: [
+    'tsc -p spec',
+    'jasmine'
+  ],
+  fix: [
+    `standard --fix "**/*.config.js"`
+  ],
+  release: [
+    `clean-release`
+  ]
+}
