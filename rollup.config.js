9746eda103876abe9929429623c3e4c00440fb92
york yao <yaoao12306@outlook.com>
2017-09-22 15:38:05 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import uglify from 'rollup-plugin-uglify'
 import resolve from 'rollup-plugin-node-resolve'
 
 export default {
-  entry: 'dist/browser.js',
-  dest: 'dist/js-split-file.min.js',
-  format: 'umd',
-  moduleName: 'SplitFile',
-  plugins: [resolve(), uglify()]
+  input: 'dist/browser.js',
+  name: 'SplitFile',
+  plugins: [resolve(), uglify()],
+  output: {
+    file: 'dist/js-split-file.min.js',
+    format: 'umd'
+  }
 }
