e345def3654301a565f003871f4f72aa6530d95f
york yao <yaoao12306@outlook.com>
2018-04-18 06:06:54 +0800

    chore: remove checkGitStatus

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,5 +1,3 @@
-const { checkGitStatus } = require('clean-scripts')
-
 const tsFiles = `"src/**/*.ts" "spec/**/*.ts" "demo/**/*.ts"`
 const jsFiles = `"*.config.js"`
 
@@ -23,8 +21,7 @@ module.exports = {
   },
   test: [
     'tsc -p spec',
-    'jasmine',
-    () => checkGitStatus()
+    'jasmine'
   ],
   fix: {
     ts: `tslint --fix ${tsFiles}`,
