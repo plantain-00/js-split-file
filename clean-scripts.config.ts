eda0a462f902193f99c9facb204f5ea59fe039b8
York Yao <me@yorkyao.com>
2020-06-26 13:48:59 +0800

    chore: stop lint js files

--- clean-scripts.config.ts
+++ clean-scripts.config.ts
@@ -1,5 +1,4 @@
 const tsFiles = `"src/**/*.ts" "spec/**/*.ts"`
-const jsFiles = `"*.config.js"`
 
 export default {
   build: [
@@ -16,7 +15,7 @@ export default {
     }
   ],
   lint: {
-    ts: `eslint --ext .js,.ts,.tsx ${tsFiles} ${jsFiles}`,
+    ts: `eslint --ext .js,.ts,.tsx ${tsFiles}`,
     export: `no-unused-export ${tsFiles}`,
     markdown: `markdownlint README.md`,
     typeCoverage: 'type-coverage -p src/tsconfig.nodejs.json --strict',
@@ -25,5 +24,5 @@ export default {
   test: [
     'ava'
   ],
-  fix: `eslint --ext .js,.ts,.tsx ${tsFiles} ${jsFiles} --fix`
+  fix: `eslint --ext .js,.ts,.tsx ${tsFiles} --fix`
 }
